540af7f32b35fa5cf3eed50b06bc3ef4049f850d
maybewp <wahyu.perwira@accenture.com>
2022-10-18 13:53:03 +0700

    Update pom.xml

--- src/test/java/com/accenture/ws/OrderAndBillingTest.java
+++ src/test/java/com/accenture/ws/OrderAndBillingTest.java
@@ -3,9 +3,12 @@ package com.accenture.ws;
 import com.accenture.ws.entity.Order;
 import com.accenture.ws.repository.OrderRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 
 import java.util.List;
@@ -13,28 +16,34 @@ import java.util.Optional;
 
 @SpringBootTest
 @ContextConfiguration
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class OrderAndBillingTest {
 
     @Autowired
     OrderRepository orderRepository;
 
     @Test
-    public void getOrderListTest() {
-        // Get All Data from Database and put to List
-        List<Order> orderList = orderRepository.findAll();
-
-        // Check Data, if > 0 will success
-        Assertions.assertThat(orderList.size()).isGreaterThan(0);
-    }
-
-    @Test
+    @org.junit.jupiter.api.Order(1)
     public void addOrderTest() {
         // Declaration Object
         Order order = new Order();
-        // Using setter
-        order.setOrderName("Hot Tea");
-        order.setPrice(10);
+        // Using setter step 1
+        order.setOrderName("Latte");
+        order.setPrice(4.5);
         order.setDiscounted(true);
+        // Check if discount == true then discountpercentage 5
+        if (order.getDiscounted()) {
+            order.setIsDiscountedPercentage(5.0);
+        } else {
+            order.setIsDiscountedPercentage(0);
+        }
+        // Save order
+        orderRepository.save(order);
+
+        // Using setter step 2
+        order.setOrderName("Cappucino");
+        order.setPrice(5.5);
+        order.setDiscounted(false);
         // Check if discount == true then discountpercentage 5
         if (order.getDiscounted()) {
             order.setIsDiscountedPercentage(5.0);
@@ -47,14 +56,24 @@ public class OrderAndBillingTest {
         // Save and get ID after insert
         Assertions.assertThat(order.getId()).isGreaterThan(0);
     }
+    @Test
+    @org.junit.jupiter.api.Order(2)
+    public void getOrderListTest() {
+        // Get All Data from Database and put to List
+        List<Order> orderList = orderRepository.findAll();
+
+        // Check Data, if > 0 will success
+        Assertions.assertThat(orderList.size());
+    }
 
     @Test
+    @org.junit.jupiter.api.Order(3)
     public void updateOrderTest() {
         // Get Data by ID
         Order order = orderRepository.findById(Long.valueOf(1)).get();
         // Using Setter
-        order.setOrderName("Cafe Latte");
-        order.setPrice(20.25);
+        order.setOrderName("Today's Brew");
+        order.setPrice(3.5);
         order.setDiscounted(true);
         // Check if discount == true then discountpercentage 5
         if (order.getDiscounted()) {
@@ -66,12 +85,14 @@ public class OrderAndBillingTest {
         orderRepository.save(order);
 
         // Update will be success and also check same expected and actual
-        Assertions.assertThat(order.getOrderName()).isEqualTo("Cafe Latte");
+        Assertions.assertThat(order.getOrderName()).isEqualTo("Today's Brew");
     }
 
     @Test
+    @org.junit.jupiter.api.Order(4)
+    @Rollback(value = false)
     public void deleteOrderTest() {
-        Long id = Long.valueOf(3);
+        Long id = Long.valueOf(1);
         Order order = orderRepository.findById(Long.valueOf(id)).get();
         orderRepository.delete(order);
 
